3687e693a0fda7b21af0a686706216f719435fed
Stéphane David <stephane.david@1-gate.com>
2017-03-28 17:11:17 +0200

    new file:   reset_sx1301.sh 	new file:   semtech/.start.sh.swp 	new file:   semtech/install_semtech.sh 	new file:   semtech/semtech.service 	new file:   semtech/start.sh 	new file:   update_hostname.sh

---
+++ update_hostname.sh
@@ -0,0 +1,15 @@
+!/bin/sh
+
+# This script is a helper to update the hostname of the gateway
+
+NEW_HOSTNAME=$(ip link show eth0 | awk '/ether/ {print $2}' | awk -F\: '{print "1gate-"$1$2$3$4$5$6}')
+#NEW_HOSTNAME=${NEW_HOSTNAME^^} #toupper
+
+CURRENT_HOSTNAME=$(hostname)
+
+echo "Updating hostname to '$NEW_HOSTNAME'..."
+hostname $NEW_HOSTNAME
+echo $NEW_HOSTNAME > /etc/hostname
+sed -i "s/$CURRENT_HOSTNAME/$NEW_HOSTNAME/" /etc/hosts
+
+exit 0
