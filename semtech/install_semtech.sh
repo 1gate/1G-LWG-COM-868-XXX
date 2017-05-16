2136700dc618c5e0f61d148e392debef046b29f6
Stéphane David <stephane.david@1-gate.com>
2017-05-16 18:29:57 +0200

    modified:   semtech/install_semtech.sh

--- semtech/install_semtech.sh
+++ semtech/install_semtech.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-echo "Install Semtech"
-
 # Stop on the first sign of trouble
 set -e
+
+if [ $UID != 0 ]; then
+    echo "ERROR: Operation not permitted. Forgot sudo?"
+    exit 1
+fi
+
+echo "Semtech Installer"
 
 # Create the required install directory and make sure it is clean
 INSTALL_DIR="/opt/semtech"
 if [ ! -d "$INSTALL_DIR" ]; then mkdir $INSTALL_DIR; fi
 pushd $INSTALL_DIR
-rm -rf *
 
 # Build lora gateway libraries
 if [ ! -d lora_gateway ]; then
@@ -21,7 +25,7 @@ else
     git pull
 fi
 
-make clean all
+make
 
 popd
 
@@ -31,11 +35,11 @@ if [ ! -d packet_forwarder ]; then
     pushd packet_forwarder
 else
     pushd packet_forwarder
-    git pull
     git reset --hard
+    git pull
 fi
 
-make clean all
+make
 
 popd
 
@@ -44,6 +48,8 @@ if [ ! -d bin ]; then mkdir bin; fi
 if [ -f ./bin/semtech_pkt_fwd ]; then rm ./bin/semtech_pkt_fwd; fi
 ln -s $INSTALL_DIR/packet_forwarder/lora_pkt_fwd/lora_pkt_fwd ./bin/semtech_pkt_fwd
 cp -f ./packet_forwarder/lora_pkt_fwd/cfg/global_conf.json.PCB_E286.EU868.basic ./bin/global_conf.json
+#cp -f ./packet_forwarder/lora_pkt_fwd/cfg/global_conf.json.PCB_E286.EU868.gps ./bin/global_conf.json
+#cp -f ./packet_forwarder/lora_pkt_fwd/cfg/global_conf.json.PCB_E286.EU868.beacon ./bin/global_conf.json
 cp -f ./packet_forwarder/lora_pkt_fwd/local_conf.json ./bin/local_conf.json
 
 # Reset gateway ID based on MAC
@@ -56,7 +62,7 @@ echo "Installation completed."
 # Start packet forwarder as a service
 cp ./start.sh $INSTALL_DIR/bin/
 cp ./semtech.service /lib/systemd/system/
-systemctl enable semtech.service
+# systemctl enable semtech.service
 
 echo "The system will reboot in 5 seconds..."
 sleep 5
