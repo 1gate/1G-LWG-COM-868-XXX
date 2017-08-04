5b6d7a12d55120111a62e1041ba9c7807a8c71c4
Stéphane David <stephane.david@1-gate.com>
2017-08-04 18:52:29 +0200

    Update configure_gateway.sh

--- ttn/configure_gateway.sh
+++ ttn/configure_gateway.sh
@@ -11,6 +11,6 @@ fi
 echo "Configure Gateway with TTN Packet Forwarder"
 
 # Create the configuration file
-./opt/ttn/bin/ttn_pkt_fwd configure --configure /etc/ttn/ttn_pkt_fwd.yml
+/opt/ttn/bin/ttn_pkt_fwd configure --config /etc/ttn/ttn_pkt_fwd.yml
 
 echo "Configuration completed."
