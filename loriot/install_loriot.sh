b3bb4e5d7293fcc59bc6a46995afd8dc8303ecbe
Stéphane David <stephane.david@1-gate.com>
2017-08-04 16:01:17 +0200

    Update install_loriot.sh

--- loriot/install_loriot.sh
+++ loriot/install_loriot.sh
@@ -34,6 +34,6 @@ cp ./start.sh $INSTALL_DIR/bin/
 cp ./loriot.service /lib/systemd/system/
 # systemctl enable loriot.service
 
-echo "The system will reboot in 5 seconds..."
-sleep 5
-shutdown -r now
+#echo "The system will reboot in 5 seconds..."
+#sleep 5
+#shutdown -r now
