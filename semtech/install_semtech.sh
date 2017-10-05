3085507a3c5fed24da6dbca261cee2a32828901c
Stephane David <stephane.david@1-gate.com.com>
2017-10-05 15:40:10 +0200

    modified:   loriot/loriot.service 	modified:   semtech/install_semtech.sh 	modified:   semtech/semtech.service 	deleted:    ttn/install_ttn_udp.sh 	modified:   ttn/ttn.service 	deleted:    ttn/uninstall_ttn_udp.sh

--- semtech/install_semtech.sh
+++ semtech/install_semtech.sh
@@ -47,9 +47,12 @@ popd
 if [ ! -d bin ]; then mkdir bin; fi
 if [ -f ./bin/semtech_pkt_fwd ]; then rm ./bin/semtech_pkt_fwd; fi
 ln -s $INSTALL_DIR/packet_forwarder/lora_pkt_fwd/lora_pkt_fwd ./bin/semtech_pkt_fwd
+
 cp -f ./packet_forwarder/lora_pkt_fwd/cfg/global_conf.json.PCB_E286.EU868.basic ./bin/global_conf.json
 #cp -f ./packet_forwarder/lora_pkt_fwd/cfg/global_conf.json.PCB_E286.EU868.gps ./bin/global_conf.json
 #cp -f ./packet_forwarder/lora_pkt_fwd/cfg/global_conf.json.PCB_E286.EU868.beacon ./bin/global_conf.json
+sed -i -e 's/"server_address": "localhost",/"server_address": "iot.semtech.com",/g' ./bin/global_conf.json
+
 cp -f ./packet_forwarder/lora_pkt_fwd/local_conf.json ./bin/local_conf.json
 
 popd
